6efbd8b4bd49d80f68afdd67a1d5b688643bacd6
Mai Quang Nhat <quangnhat224@gmail.com>
2019-06-17 19:27:01 +0300

    refactor code for 1.3

--- part1/src/index.js
+++ part1/src/index.js
@@ -14,7 +14,7 @@ const Content = (props) => {
   return (
     <div>
       <div>Content:</div>
-      <div>{props.part}</div>
+      <div>{props.name}</div>
       <div>Excercises:</div>
       <div>{props.excercises}</div>
     </div>
@@ -32,20 +32,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} excercises={exercises1}/>
-      <Content part={part2} excercises={exercises2}/>
-      <Content part={part3} excercises={exercises3}/>
-      <Total excercises={exercises1 + exercises2 + exercises3} />
+      <Content name={part1.name} excercises={part1.exercises}/>
+      <Content name={part2.name} excercises={part2.exercises}/>
+      <Content name={part3.name} excercises={part3.exercises}/>
+      <Total excercises={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
