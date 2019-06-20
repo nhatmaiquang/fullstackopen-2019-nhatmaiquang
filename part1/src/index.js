f2dc949feefa1b2ae6dc059a3a626e911ad7e7b0
Mai Quang Nhat <quangnhat224@gmail.com>
2019-06-20 11:57:17 +0300

    Update index.js
    1.6

--- part1/src/index.js
+++ part1/src/index.js
@@ -9,9 +9,22 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h2>give feedback</h2>
+      <button onClick={ () => setGood(good + 1)}>
+        Good
+      </button>
+      <button onClick={ () => setNeutral(neutral + 1)}>
+        Neutral
+      </button>
+      <button onClick={ () => setBad(bad + 1)}>
+        Bad
+      </button>
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
     </div>
-  )
+  );
 }
 
 ReactDOM.render(<App />, 
