a42c625184c7c0dcd5c86dae0d34e94727d7c2b9
Mai Quang Nhat <quangnhat224@gmail.com>
2019-06-21 10:41:22 +0300

    anecdote

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,72 +1,63 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistic = (props) => {
-  if (props.text === "positive") {
-    return (
-      <tr>
-        <td>{props.text}</td>
-        <td>{props.value}%</td>
-      </tr>
-    );
-  }
-  return(
-    <tr>
-      <td>{props.text}</td>
-      <td>{props.value}</td>
-    </tr>
-  );
-}
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
 
-const Button = (props) => {
-  return(
-    <button onClick={props.handleClick}>{props.text}</button>
-  );
-}
+  const [points, setPoints] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0});
 
-const Statistics = (props) => {
-  const all = props.good + props.neutral + props.bad;
-  const average = (props.good - props.bad)/(props.good + props.neutral + props.bad);
-  const positive = 100*props.good/(props.good + props.neutral + props.bad);
+  // console.log(points);
+  // console.log(selected);
 
-  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
-    return (
-      <div>No feedback given</div>
-    );
+  const vote = (index) => {
+    const copy = {...points};
+    copy[index] += 1;  
+    setPoints(copy);
+    // console.log(index);
+    // console.log(copy);
   }
 
-  return(
-    <table>
-      <tbody>
-        <Statistic text="good" value ={props.good} />
-        <Statistic text="neutral" value ={props.neutral} />
-        <Statistic text="bad" value ={props.bad} />
-        <Statistic text="all" value ={all} />
-        <Statistic text="average" value ={average} />
-        <Statistic text="positive" value ={positive} />
-      </tbody>
-    </table>
-  );
-}
+  const mostVoted = (obj) => {
+    let arr = Object.values(obj);
+    let max = Math.max(...arr);
+    // console.log(`max value: ${max}`);
+    // console.log(getKeyByValue(obj, max));
+    return getKeyByValue(obj, max);
+  }
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+  const randomNumber = () => {
+    return ((Math.floor((Math.random() * 6))));
+  };
+  
 
   return (
     <div>
-      <h2>give feedback</h2>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h2>statistics</h2>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <h2>Anecdote of the day</h2>
+      <div>{props.anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
+      <button onClick={() => vote(selected)}>vote</button>
+      <button onClick={() => setSelected(randomNumber())} >next anecdote</button>
+      <h2>Anecdote with most votes</h2>
+      <div>{props.anecdotes[mostVoted(points)]}</div>
+      <div>has {points[[mostVoted(points)]]} votes</div>
     </div>
-  );
+    
+  )
 }
 
-ReactDOM.render(<App />, 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
---
+++ part1/unicafe/index.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Statistic = (props) => {
+  if (props.text === "positive") {
+    return (
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}%</td>
+      </tr>
+    );
+  }
+  return(
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
+}
+
+const Button = (props) => {
+  return(
+    <button onClick={props.handleClick}>{props.text}</button>
+  );
+}
+
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad;
+  const average = (props.good - props.bad)/(props.good + props.neutral + props.bad);
+  const positive = 100*props.good/(props.good + props.neutral + props.bad);
+
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <div>No feedback given</div>
+    );
+  }
+
+  return(
+    <table>
+      <tbody>
+        <Statistic text="good" value ={props.good} />
+        <Statistic text="neutral" value ={props.neutral} />
+        <Statistic text="bad" value ={props.bad} />
+        <Statistic text="all" value ={all} />
+        <Statistic text="average" value ={average} />
+        <Statistic text="positive" value ={positive} />
+      </tbody>
+    </table>
+  );
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
