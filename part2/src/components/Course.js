caab6c9507a22a8f8f44cde499a9418a50133ea3
Mai Quang Nhat <quangnhat224@gmail.com>
2019-06-25 14:29:06 +0300

    2.2 total component

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -2,12 +2,21 @@ import React from 'react';
 import Header from './Header';
 import Content from './Content';
 
+const Total = (props) => {
+
+    const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+  
+    return (
+      <div><b>Total exercises: {total}</b></div>
+    );
+}
+
 const Course = ({ course }) => {
     return (
         <div>
             <Header course={course.name} />
             <Content parts={course.parts} />
-            {/* <Total parts={course.parts} /> */}
+            <Total parts={course.parts} />
         </div>
     );
 }
--- part2/src/index.js
+++ part2/src/index.js
@@ -2,15 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Course from './components/Course';
 
-const Total = (props) => {
-
-  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
-
-  return (
-    <div>Total exercises: {total}</div>
-  );
-}
-
 const App = () => {
   const course = {
     name: 'Half Stack application development',
