1a426d0901eb35c188d9450785efc43be0541841
Mai Quang Nhat <quangnhat224@gmail.com>
2019-06-26 11:34:53 +0300

    2.3 reduce

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -2,13 +2,23 @@ import React from 'react';
 import Header from './Header';
 import Content from './Content';
 
-const Total = (props) => {
+// const Total = (props) => {
 
-    const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+//     const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
   
+//     return (
+//       <div><b>Total exercises: {total}</b></div>
+//     );
+// }
+
+const Total = (props) => {
+    console.log(props);
+    const total = props.parts.reduce((a, b) =>{return a + b.exercises},0);
+    console.log(total);
     return (
-      <div><b>Total exercises: {total}</b></div>
+        <div><b>Total exercises: {total}</b></div>
     );
+    
 }
 
 const Course = ({ course }) => {
