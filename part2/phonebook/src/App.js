cd3e9a75fc4c9e2ffe5d5fe84d71eace54665541
Mai Quang Nhat <quangnhat224@gmail.com>
2019-09-16 11:21:42 +0300

    2.6 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,36 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+  const Persons = ({ persons }) => {
+    return persons.map(person => (
+      <div key={person.name}>
+        <p>{person.name}</p>
+      </div>
+    ));
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input
+                  type="text"
+                  value={newName}
+                  onChange={event => setNewName(event.target.value)}
+                />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={() => setPersons([...persons, {name: newName}])} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Persons persons={persons}/>
     </div>
   )
 }
